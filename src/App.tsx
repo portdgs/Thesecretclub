80f32c996e4ab4ec46b8bedc5b31e6c0fd1194e7
portdgs <portdgs@gmail.com>
2026-03-06 01:42:06 -0300

    style: increase navbar headline font size

--- src/App.tsx
+++ src/App.tsx
@@ -448,7 +448,7 @@ export default function App() {
             </div>
 
             <div className="flex-1 flex justify-center hidden lg:flex">
-              <h1 className="text-lg font-serif font-light tracking-tight text-white/90">
+              <h1 className="text-xl sm:text-2xl font-serif font-light tracking-tight text-white/95">
                 A arte do <span className="text-primary italic">encontro</span> em sua <span className="text-primary italic">melhor</span> forma
               </h1>
             </div>
